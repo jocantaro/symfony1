5871d99fef7ea9db44be8a8082087ab8b3ebf29b
jocantaro <ricardolopeztoledo@gmail.com>
2016-02-05 20:52:21 +0000

    user controller 3

--- symfony1/src/testBundle/Controller/UserController.php
+++ symfony1/src/testBundle/Controller/UserController.php
@@ -8,7 +8,14 @@ class UserController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('testBundle:Default:userindex.html.twig');
+
+        $em = $this->getDoctrine()->getManager();
+
+
+        $users = $em->getRepository('testBundle:Users')->findAll();
+
+
+        return $this->render('testBundle:Default:userindex.html.twig', array ('users'=>$users));
     }
 
 
@@ -17,4 +24,34 @@ class UserController extends Controller
         return $this->render('testBundle:Default:userarticles.html.twig', array ('page'=>$page) );
 
     }
+
+    public function addAction ($page){
+
+        return $this->render('testBundle:Default:userarticles.html.twig', array ('page'=>$page) );
+
+    }
+
+    public function editAction ($page){
+
+        return $this->render('testBundle:Default:userarticles.html.twig', array ('page'=>$page) );
+
+    }
+
+    public function deleteAction ($page){
+
+        return $this->render('testBundle:Default:userarticles.html.twig', array ('page'=>$page) );
+
+    }
+
+    public function viewAction ($id){
+
+        $repository = $this->getDoctrine()->getRepository('testBundle:Users');
+
+        //$user = $repository->find($id);
+        $user = $repository->findOneBy(array('id'=>$id));
+
+        return $this->render('testBundle:Default:userview.html.twig', array ('user'=>$user) );
+
+    }
+
 }
---
+++ symfony1/src/testBundle/Entity/Users.php
@@ -0,0 +1,253 @@
+<?php
+
+namespace testBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Users
+ *
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="testBundle\Repository\UsersRepository")
+ */
+class Users
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=50)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=100)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=100, nullable=true)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=100)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="role", type="string", columnDefinition="ENUM('ROLE_ADMIN','ROLE_USER')", length=50)
+     */
+    private $role;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return Users
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return Users
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return Users
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Users
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return Users
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     *
+     * @return Users
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     *
+     * @return Users
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return bool
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+}
+
